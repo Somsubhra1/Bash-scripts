9ba906cfb24bb463748e5207de52c06ffa4b8420
Shubhamdutta2000 <dondutta2000@gmail.com>
2022-10-03 21:10:29 +0530

    feat: added parse current date time utility func

---
+++ parseDateTime.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+Day=`date +%d`
+Month=`date +%m`
+Year=`date +%Y`
+Hour=`date +%H`
+Minute=`date +%M`
+Second=`date +%S`
+echo `date`
+echo "Current Date is: $Day-$Month-$Year"
+echo "Current Time is: $Hour:$Minute:$Second"
