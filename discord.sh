c773d3b364165f70d1c454f88cf69bdf4f0ceef9
ishitadutta1306 <89701214+ishitadutta1306@users.noreply.github.com>
2022-10-16 12:40:11 +0530

    Create discord.sh
    This script will install Discord on your system.

---
+++ discord.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# This script will install Discord on your system
+
+# Install Discord
+sudo snap install discord
+
+# Install Discord themes
+# Discord theme: Discord Black
+sudo snap install discord-black-theme
+
+# Discord theme: Discord Dark
+sudo snap install discord-dark-theme
+
+
+# Install Discord plugins
+# Discord plugin: Discord Rich Presence
+sudo snap install discord-rich-presence
